9c0b1803c81f7b0d183dc6fb8ad314346961fc7c
yarik-vv <yarik.vashchuk@gmail.com>
2017-07-16 22:14:33 +0300

    created webpack config

---
+++ webpack.config.js
@@ -0,0 +1,96 @@
+'use strict';
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+   context: __dirname + '/src',
+
+   entry: './index',
+
+   output: {
+      path: __dirname + '/www',
+      publicPath: '/',
+      filename: '[name].js'
+   },
+
+   watch: NODE_ENV == 'development',
+   watchOptions: {
+      aggregateTimeout: 100,
+      ignored: /node_modules/
+   },
+
+   resolve: {
+      modules: ['node_modules'],
+      extensions: ['.js', '.css']
+   },
+
+   resolveLoader: {
+      modules: ['node_modules'],
+      moduleExtensions: ['-loader'],
+      extensions: ['.js']
+   },
+
+   module: {
+      rules: [{
+         test: /\.scss$/,
+         include: __dirname + '/src',
+         use: ExtractTextPlugin.extract({
+            fallback: 'style',
+            use:['css', 'postcss', 'sass']
+         })
+      }, 
+      {
+         test:   /\.(png|jpg|svg|otf)$/,
+         exclude: /(node_modules)/,
+         loader: 'file-loader?name=[path][name].[ext]',
+      }]
+   },
+   
+   plugins: [
+      new webpack.NoEmitOnErrorsPlugin(),
+      new webpack.DefinePlugin({
+         NODE_ENV: JSON.stringify(NODE_ENV)
+      }),
+      new CleanWebpackPlugin(__dirname + '/www/*'),
+      new HtmlWebpackPlugin({
+         filename: 'index.html',
+         template: 'index.html',
+      })
+   ],
+
+   devServer: {
+      port: 9000,
+      contentBase: __dirname + '/www',
+      watchContentBase: true,
+      compress: true,
+      inline: true,
+      hot: true
+   }
+};
+
+if (NODE_ENV == 'production') {
+   module.exports.plugins.push(
+      new webpack.LoaderOptionsPlugin({
+         minimize: true,
+         debug: false
+      }),
+      new webpack.DefinePlugin({
+         'process.env': {
+            'NODE_ENV': JSON.stringify('production')
+         }
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+         beautify: false,
+         mangle: {
+            screw_ie8: true,
+            keep_fnames: true
+         },
+         compress: {
+            screw_ie8: true
+         },
+         comments: false
+      })
+   );
+};
