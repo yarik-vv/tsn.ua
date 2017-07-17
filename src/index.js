d97cc6817320b09ab2b238de604ff5b7c33e62cc
yarik-vv <yarik.vashchuk@gmail.com>
2017-07-17 11:16:50 +0300

    slider

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 "use strict";
 
 import scss from './scss/index.scss';
-import icon from './images/share.jpg';
+import share_img from './images/share.jpg';
+import video1 from './video.mp4';
+
+//import slide1 from './images/slide1.jpg';
+import slide2 from './images/slide2.jpg';
+import slide3 from './images/slide3.jpg';
+import slide4 from './images/slide4.jpg';
+import slide5 from './images/slide5.jpg';
+
